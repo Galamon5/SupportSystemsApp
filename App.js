b964855de7c3aaf0ee1fe89bc088d4000ae57f45
Carlitos Carmatch <galamon5@hotmail.com>
2018-10-24 18:59:37 -0500

    Testing Webview

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import {  View } from 'react-native';
+import {  View, StyleSheet, WebView, Text} from 'react-native';
+import {TabBarIcon} from './components/TabBarIcon';
 
-export default class FixedDimensionsBasics extends Component {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      number: 2
+    }
+  }
   render() {
     return (
-      <View style={{ flex:1, backgroundColor: 'red'}}>
-        <View style={{ flex:1, backgroundColor: 'powderblue'}} />
-        <View style={{ flex:2,   backgroundColor: 'skyblue'}} />
-        <View style={{ flex:3, backgroundColor: 'steelblue'}} />
-      </View>
+
+          <View style = {styles.box2}>
+            <TabBarIcon/>
+          </View>
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: 'red'
+  },
+  box1: {
+    flex:1,
+    backgroundColor: 'red'
+  },
+  box2: {
+    flex:1,
+    backgroundColor: 'skyblue'
+  }
+});
+export default App;
